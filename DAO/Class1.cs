adebeb4cd202a2c02cf83fc0b78f7333f36b631d
Phan Anh Trúc <anhtrucphanit@gmail.com>
2018-11-18 17:05:49 +0700

    1

---
+++ DAO/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Tiếp cận với database
+namespace DAO
+{
+    public class Class1
+    {
+
+    }
+}
