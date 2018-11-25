c2bf0d9f1254d2bb6ab91d3bc5a8696bb3220553
Phan Anh Trúc <anhtrucphanit@gmail.com>
2018-11-25 18:30:04 +0700

    hii

---
+++ QLTV_Project/BUS/LoaiDocGia_BUS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using DTO;
+using DAO;
+namespace BUS
+{
+    public class LoaiDocGia_BUS
+    {
+        LoaiDocGia_DAO LoaiDocGia_dao = new LoaiDocGia_DAO();
+        public DataTable getLoaiDocGia()
+        {
+            return LoaiDocGia_dao.getLoaiDocGia();
+        }
+
+        public DataTable LoadCboLoaiDG()
+        {
+            return LoaiDocGia_dao.LoadCbo();
+        }
+    }
+}
---
+++ QLTV_Project/BUS/NCC_BUS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DTO;
+using DAO;
+namespace BUS
+{
+    public class NCC_BUS
+    {
+        NCC_DAO NCC_dao = new NCC_DAO();
+        public DataTable getNCC()
+        {
+            return NCC_dao.getNCC();
+        }
+    }
+}
