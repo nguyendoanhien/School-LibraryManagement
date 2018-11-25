c4712cd76cf8db8b151a6778568d4bd153e6d68c
Nguyen Doan Hien <toilati123vn@gmail.com>
2018-11-25 22:41:00 +0700

    ++Updated NguoiDung

---
+++ QLTV_Project/DTO/NguoiDung.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    
+    public class NguoiDung
+    {
+        public int MaNguoiDung { get; set; }
+
+        public string HoVaTen { get; set; }
+
+        public bool? GioiTinh { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime? NgayTao { get; set; }
+
+        
+        public NguoiDung(int maNguoiDung, string hoVaTen, bool? gioiTinh, string email, DateTime? ngayTao)
+        {
+            MaNguoiDung = maNguoiDung;
+            HoVaTen = hoVaTen;
+            GioiTinh = gioiTinh;
+            Email = email;
+            NgayTao = ngayTao;
+        }
+    }
+
+    
+}
