4bffb2397806940c07aabf6fe7286473421ba0ea
Phan Anh Trúc <anhtrucphanit@gmail.com>
2018-12-04 15:04:54 +0700

    thêm

---
+++ QLTV_Project/BUS/DauSachLoai_BUS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using DTO;
+using DAO;
+namespace BUS
+{
+    public class DauSachLoai_BUS
+    {
+        DauSach_DAO dausach_dao = new DauSach_DAO();
+        public DataSet LoadComBoBoxDauSachLoai(string tenbang)
+        {
+            return dausach_dao.LoadComBoDauSach(tenbang);
+        }
+    }
+}
