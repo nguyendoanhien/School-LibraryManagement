2082e798187fc63d848255b7051ee59c3768ab36
Phan Anh Trúc <anhtrucphanit@gmail.com>
2018-12-08 16:50:31 +0700

    ABC

---
+++ QLTV_Project/BUS/PhieuTra_BUS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+namespace BUS
+{
+    public class PhieuTra_BUS
+    {
+        PhieuTra_DAO phieuTra_DAO = new PhieuTra_DAO();
+        public int MaPhieuTra()
+        {
+            return phieuTra_DAO.MaPhieuNhap();
+        }
+    }
+}
